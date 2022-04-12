350c4a22e325537a6463e2500d2eb893d448e974
Ardeshir Eshghi <ardi.eshghi@gmail.com>
2022-04-12 01:39:50 +0100

    Updates `AccessTokenRepository`
    Implement `get_token` which reads user or bot access tokens
    from S3 based on team and user ids

--- decisions_app/frameworks/database/access_token_repo.py
+++ decisions_app/frameworks/database/access_token_repo.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+import json
 from botocore.exceptions import ClientError
 
 DECISIONS_S3_BUCKET = os.environ["DECISIONS_S3_BUCKET"]
@@ -13,14 +14,32 @@ class AccessTokenRepository:
     def __init__(self, s3_client=None):
         self.client = s3_client if s3_client else create_s3_client()
 
-    def get_token(self, team_id, user_id):
-        pass
+    def get_token(self, type, team_id, user_id=None):
+        storage_key = self._get_storage_key(
+            type=type, team_id=team_id, user_id=user_id)
+        response = self.client.get_object(
+            Bucket=DECISIONS_S3_BUCKET,
+            Key=storage_key,
+        )
+        parsed_response = response.get('Body').read().decode("utf-8")
+        return parsed_response
 
-    def save_token(self, access_token, user_id, team_id):
+    def save_token(self, type, access_token, team_id, user_id=None):
+        storage_key = self._get_storage_key(
+            type=type, team_id=team_id, user_id=user_id)
         response = self.client.put_object(
             Body=access_token,
             Bucket=DECISIONS_S3_BUCKET,
-            Key=f"/{team_id}/{user_id}/token",
+            Key=storage_key,
         )
 
-        print(response)
+        return response
+
+    def _get_storage_key(self, type, team_id, user_id=None):
+        if type == 'user':
+            return f"/{team_id}/{user_id}/{self._token_name(type)}"
+        else:
+            return f"/{team_id}/{self._token_name(type)}"
+
+    def _token_name(self, type):
+        return 'user_token' if type == 'user' else 'bot_token'
